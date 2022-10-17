5c0c5d57dfa673693a1e5e1e4fc71c25bfc1f837
Karen A Nolan <Karen.Nolan@TUDublin.ie>
2022-10-17 17:08:29 +0100

    First commit of code for CA2

---
+++ PlanetWeight/Pages/PlanetWeight.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PlanetWeights.Pages
+{
+    public class PlanetWeightsModel : PageModel
+    {
+        [BindProperty]      // bound on POST request
+        public PlanetWeight planetWeight { get; set; }
+
+
+        // POST, validate
+        public IActionResult OnPost()
+        {
+            // extra validation (probably not that useful, but might be were additional validation can be added)
+            if (planetWeight.weight == 0.0)
+            {
+                ModelState.AddModelError("", "You need to enter your weight");
+            }
+            return Page();
+        }
+    }
+}
---
+++ PlanetWeight/PlanetWeight.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace PlanetWeights
+{
+    /*
+     * List of Planets
+     */
+    public enum Planets
+    {
+        [Display(Name = "Mercury")] mercury,
+        [Display(Name = "Venus")] venus,
+        [Display(Name = "The Moon")] moon,
+        [Display(Name = "Mars")] mars,
+        [Display(Name = "Jupiter")] jupiter,
+        [Display(Name = "Saturn")] saturn,
+        [Display(Name = "Uranus")] uranus,
+        [Display(Name = "Neptune")] neptune,
+        [Display(Name = "Pluto")] pluto
+    }
+
+
+    //Class to present to the Internet
+    public class PlanetWeight
+    {
+        private List<double> planetsSurfaceGravity = new List<double> { 0.38, 0.91, 1.62, 0.38, 2.34, 0.93, 0.92, 1.12 };
+
+        public const int weightMin = 1;
+        public const int WeightMax = 500;
+
+        [Range(weightMin, WeightMax, ErrorMessage = "Invalid weight - Only Available from 1 to 500 lbs")]
+        [DisplayName("Enter Your Weight (KGs/LBs):")]
+        public double weight { get; set; }
+        
+        [DisplayName("Select A Planet:")]
+        public Planets planet { get; set; }
+
+        // calculate weight mercury
+        [DisplayName("Your Weight on that Planet:")]
+        public double weightOnPlanet
+        {
+            get
+            {
+                double weightConverted = this.weight;
+
+                if (planet.Equals(Planets.mercury))
+                {
+                    weightConverted = this.weight * planetsSurfaceGravity[(int)Planets.mercury];
+                }
+                else if (planet.Equals(Planets.venus))
+                {
+                    weightConverted = this.weight * planetsSurfaceGravity[(int)Planets.venus];
+                }
+                else if (planet.Equals(Planets.moon))
+                {
+                    weightConverted = this.weight * planetsSurfaceGravity[(int)Planets.moon];
+                }
+                else if (planet.Equals(Planets.mars))
+                {
+                    weightConverted = this.weight * planetsSurfaceGravity[(int)Planets.mars];
+                }
+                else if (planet.Equals(Planets.jupiter))
+                {
+                    weightConverted = this.weight * planetsSurfaceGravity[(int)Planets.jupiter];
+                }
+                else if (planet.Equals(Planets.saturn))
+                {
+                    weightConverted = this.weight * planetsSurfaceGravity[(int)Planets.saturn];
+                }
+                else if (planet.Equals(Planets.uranus))
+                {
+                    weightConverted = this.weight * planetsSurfaceGravity[(int)Planets.uranus];
+                }
+                else if (planet.Equals(Planets.neptune))
+                {
+                    weightConverted = this.weight * planetsSurfaceGravity[(int)Planets.neptune];
+                }
+                else if (planet.Equals(Planets.pluto))
+                {
+                    weightConverted = this.weight * planetsSurfaceGravity[(int)Planets.pluto];
+                }
+                return weightConverted;
+            }
+        }
+    }
+}
