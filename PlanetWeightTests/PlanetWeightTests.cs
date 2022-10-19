600af5a7499ebc13005571094ff57021e7576cd3
x00204385 <x00204385@mytudublin.ie>
2022-10-19 16:29:39 -0700

    Added framework unit tests

---
+++ PlanetWeightTests/PlanetWeightTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PlanetWeights;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanetWeights.Tests
+{
+    [TestClass()]
+    public class PlanetWeightTests
+    {
+        [TestMethod()]
+        public void weightOnPlanetTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
